a3ebc576e6fe6e0f2a92eeb8ab8861e268bd4448
“CristianReyes2023” <cristianlrm2019@gmail.com>
2023-10-15 11:08:13 -0500

    feat: :sparkles: IUnitOfWork

--- Core/Interfaces/IUnitOfWork.cs
+++ Core/Interfaces/IUnitOfWork.cs
@@ -3,10 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace Core.Interfaces
+namespace Core.Interfaces;
+public interface IUnitOfWork 
 {
-    public interface IUnitOfWork
-    {
-        
-    }
+    ICiudad Ciudades {get;}
+    IContactoPersona ContactoPersonas {get;}
+    IDepartamento Departamentos {get;}
+    IDetalleMovInventario DetalleMovInventarios {get;}
+    IFactura Facturas {get;}
+    IFormaPago FormaPagos {get;}
+    IInventario Inventarios {get;}
+    IMarca Marcas {get;}
+    IMovimientoInventario MovimientoInventarios {get;}
+    IPais Paises {get;}
+    IPersona Personas {get;}
+    IPresentacion Presentaciones {get;}
+    IProducto Productos {get;}
+    IRolPersona RolPersonas {get;}
+    ITipoContacto TipoContactos {get;}
+    ITipoDocumento TipoDocumentos {get;}
+    ITipoMovInventario TipoMovInventarios {get;}
+    ITipoPersona TipoPersonas {get;}
+    IUbicacionPersona UbicacionPersonas {get;}
+
+    Task<int> SaveAsync();
 }
